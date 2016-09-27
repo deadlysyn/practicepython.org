9f149c34a6f6b52b227b3e53aca6267087de9562
DeadlySyn <unixdoes@gmail.com>
2016-09-26 20:24:14 -0400

    exercise 6

---
+++ 006/ex6.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#
+# practicepython.org exercise 6:
+# Ask for string and determine if it is a palindrome.
+
+
+test_str = input('Enter string to test: ')
+
+rev_str = test_str[::-1]
+
+if test_str.lower() == rev_str.lower():
+    print('Congrats, it looks like a palindrome!')
+else:
+    print('Sorry, that is not a palindrome.')
