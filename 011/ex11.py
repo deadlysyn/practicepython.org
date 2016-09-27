a6f995e40b02ac3591cc23b9edf5cc43b7883d25
DeadlySyn <unixdoes@gmail.com>
2016-09-26 23:09:06 -0400

    exercise 11

--- 011/ex11.py
+++ 011/ex11.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python
 #
-# practicepython.org exercise 10:
-# redo exercise 5 a bit differently
-
-import random
+# practicepython.org exercise 11:
+# ask for number and determine if prime
 
 
-# generate two random lists
-a = random.sample(range(100), random.randint(1, 100))
-b = random.sample(range(100), random.randint(1, 100))
+def get_num(prompt='Number? '):
+    """prompt for a number"""
+    _num = 0
+    while True:
+        try:
+            _num = int(input(prompt))
+        except ValueError:
+            print('Was that a number? Try again!')
+            continue
+        else:
+            break
+    return _num
 
-c = set([x for x in a if x in b])
 
-print('a: ', a, '\n')
-print('b: ', b, '\n')
-print('c: ', c)
+def is_prime(num):
+    """given a number, return True if prime else False"""
+    for x in range(2, num + 1):
+        if num % x == 0:
+            return False
+    return True
+
+
+num = get_num()
+
+if is_prime(num):
+    print('Primo!')
+else:
+    print('Booooooooring.')
