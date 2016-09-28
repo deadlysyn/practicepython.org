4d9f9d1587efe07da5f13fafbadfaba034b1470d
DeadlySyn <unixdoes@gmail.com>
2016-09-28 01:00:32 -0400

    clean slate

--- 016/ex16.py
+++ 016/ex16.py
@@ -1,19 +1,4 @@
 #!/usr/bin/env python
 #
-# practicepython.org exercise 15:
-# reverse word order in strings
-
-
-def reverse_string(string):
-    """pass in a string, get the reverse!"""
-    # the magic one-liner!
-    return ' '.join(string.split()[::-1])
-# ugly attempt #1
-#    new = string.split()[::-1]
-#    for x in new:
-#        print(x, end=' ')
-#    print('\n')
-
-
-s = input('Enter a string: ')
-print(reverse_string(s))
+# practicepython.org exercise 16:
+# password generator
